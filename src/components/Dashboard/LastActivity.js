6dec0e74d0f474e58a3461a966f24de5be14b642
Przemysław Kuca <evildemage@gmail.com>
2021-01-25 20:19:03 +0100

    Alerts styled

--- src/components/Dashboard/LastActivity.js
+++ src/components/Dashboard/LastActivity.js
@@ -8,12 +8,14 @@ class LastActivity extends React.Component {
             <Wrapper>
                 <Heading>Aktywność agentów</Heading>
                 <Container>
-                    {this.props.filteredCommunication.length === 0 && <p>Brak aktywności!</p> }
-                    {this.props.filteredCommunication.map(({message, date, agentId, type}, number) => (
+                    {this.props.filteredCommunication.length === 0 && <p>Brak aktywności!</p>}
+                    {this.props.filteredCommunication.reverse().map(({message, date, agentId, type}, number) => (
                         <Box key={number}>
-                            <AgentHeading>{type} {agentId}</AgentHeading>
-                            <p style={{fontSize: '14px'}}>{message}</p>
-                            <DateParagraph>{date.substr(0, 19).replace(/T/g, ' ')}</DateParagraph>
+                            <TitleWrapper>
+                                <div><AgentHeading><AlertIcon  type={type}/>{type} {agentId}</AgentHeading></div>
+                                <div><DateParagraph>{date.substr(0, 19).replace(/T/g, ' ')}</DateParagraph></div>
+                            </TitleWrapper>
+                            <p style={{fontSize: '14px', textAlign: "justify"}}>{message}</p>
                             <hr/>
                         </Box>
                     ))}
@@ -27,7 +29,7 @@ const Wrapper = styled.div`
   height: 60%;
   color: black;
   background: #d0d0d04a;
-  text-align: center;
+  // text-align: center;
   padding: 1rem;
   display: grid;
   justify-items: center;
@@ -38,8 +40,18 @@ const Heading = styled.h1`
   margin-bottom: 0rem;
 `;
 
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0 !important;
+`;
+
 const AgentHeading = styled.h4`
   font-weight: 900;
+`;
+
+const DateParagraph = styled.p`
+  font-size: 12px;
 `;
 
 const Container = styled.div`
@@ -49,14 +61,21 @@ const Container = styled.div`
   overflow-x: hidden;
 `;
 
-const DateParagraph = styled.p`
-  font-size: 12px;
-`;
-
 const Box = styled.div`
+  padding: 0 15px;
+
   :last-child hr {
     display: none;
   }
 `;
+
+const AlertIcon = styled.div`
+    height: 12px;
+    width: 12px;
+    border-radius: 12px;
+    background-color: ${props => props.type == 'Straż pożarna' ? "#009EFA" : props.type == 'Czujnik' ? "#FF8066" : "#00AF54"};
+    display: inline-block;
+    margin-right: 5px;
+    `
 
 export default LastActivity;
